2bcade7284ff8ff536d2d9b98e8dd1b6460a3a5f
Luis Eduardo <161743182+LuisEduardoS23@users.noreply.github.com>
2025-07-19 23:18:42 -0300

    Ajustando main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import src.controller.Sistema;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("oii hihi, testando o git aqui");
 
         Sistema sistema = new Sistema();
 
