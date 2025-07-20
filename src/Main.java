5176b79a3d83f15dd55d58e149544ee3a932bdf1
Luis Eduardo <luisesoares4@gmail.com>
2025-07-19 23:16:51 -0300

    Teste

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import src.controller.Sistema;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("oii hihi, testando o git aqui");
 
         Sistema sistema = new Sistema();
 
